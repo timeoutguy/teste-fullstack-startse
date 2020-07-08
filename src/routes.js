42e4b5172fe41560d7ebe4297d9ce5615630c5ae
timeoutguy <marcus.dev@protonmail.com>
2020-07-08 13:11:59 -0300

    Rotas da API

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,7 @@ routes.get('/courses', CourseController.index);
 routes.get('/courses/find', CourseController.search);
 routes.get('/courses/:id', CourseController.show);
 routes.post('/courses', CourseController.store);
+routes.put('/courses/:id', CourseController.update);
+routes.delete('/courses/:id', CourseController.delete);
 
 export default routes;
