91987908be0864d1cf47a15be08da4e516b6a2b8
timeoutguy <marcus.dev@protonmail.com>
2020-07-08 14:30:10 -0300

    Variaveis de ambiente

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 import './database/index';
 import routes from './routes';
+
+dotenv.config();
 
 const app = express();
 
@@ -10,4 +13,4 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.APP_PORT || 3333);
