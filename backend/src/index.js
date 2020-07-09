4d88ade8a8d428b00943b35135ade495d2ea560d
timeoutguy <marcus.dev@protonmail.com>
2020-07-09 10:57:29 -0300

    CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import './database/index';
 import routes from './routes';
@@ -9,6 +10,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
