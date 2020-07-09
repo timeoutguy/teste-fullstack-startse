0b5dce7a693da8b4b0502274b32ff0fdff6fb706
timeoutguy <marcus.dev@protonmail.com>
2020-07-09 17:36:55 -0300

    Listagem de cursos e tratamento de erro

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,17 +1,33 @@
 import React, { useState, useEffect} from 'react'
+import { toast, ToastContainer } from 'react-toastify';
 import { Container } from 'react-bootstrap'
 import moment from 'moment'
 import Search from '../../components/Search'
 import CourseInfo from '../../components/CourseInfo'
 import api from '../../services/api'
 
+import 'react-toastify/dist/ReactToastify.min.css'; 
+
 export default function Main() {
 
   const [courses, setCourse] = useState([]);
 
-  let getCourse = (filter = '')  => {
+  let getCourse = async (filter = '')  => {
     const search = filter ?`/find?q=${filter}` : ''
-    api.get(`/courses${search}`).then(res => setCourse(res.data)); 
+
+    try {
+      await api.get(`/courses${search}`).then(res => setCourse(res.data)); 
+    } catch (err) {
+      toast.error('Um erro ocorreu! 😦', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
   }
 
   let handleSearch = (search) => {
@@ -22,13 +38,18 @@ export default function Main() {
     getCourse();
   }, [])
 
+  console.log(courses)
+
   return (
+    <>
     <Container className="mt-4">
       <h1> Cursos </h1>
       <Search handleSearch={handleSearch}/>
       <div className="mt-4">
         {courses.map((course, i) => {
-          return (<CourseInfo key={i}
+          return (<CourseInfo 
+            key={i}
+            id={course.id}
             title={course.title}
             subtitle={course.subtitle}
             description={course.description}
@@ -38,5 +59,7 @@ export default function Main() {
         })}
       </div>
     </Container>
+    <ToastContainer />
+    </>
   )
 }
