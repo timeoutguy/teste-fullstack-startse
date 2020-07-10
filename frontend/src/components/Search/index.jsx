5173e22475082b798fa0edfb872897da4fee451b
timeoutguy <marcus.dev@protonmail.com>
2020-07-10 14:49:16 -0300

    Tecla de atalho para pesquisar

--- frontend/src/components/Search/index.jsx
+++ frontend/src/components/Search/index.jsx
@@ -6,11 +6,21 @@ export default function Search(props) {
 
   const [search, setSearch] = useState('');
 
+  const keyHandler = (e) => {
+    if(e.key === 'Enter'){
+      props.handleSearch(search)  
+    }
+
+    if(search === ''){
+      props.handleSearch();
+    }
+  }
+
   return (
     <div>
       <Row>
         <Col xs={10} sm={10} md={11}>
-          <FormControl onChange={e => setSearch(e.target.value)} value={search} id="title" placeholder="Procure por um curso" />
+          <FormControl onKeyUp={keyHandler} onChange={e => setSearch(e.target.value)} value={search} id="title" placeholder="Procure por um curso" />
         </Col>
         <Col xs={2} sm={2} md={1}>
           <Button variant="primary" onClick={() => props.handleSearch(search)} > <FaSearch /> </Button>
